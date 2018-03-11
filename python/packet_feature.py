c18c61c682a97a8ada20aee795636dc513aec629
shuo.jiang <jimmyahacker@gmail.com>
2018-03-11 01:56:54 -0500

    1.refine the previous test:remove old test case and add anew, add correct answer for the test case, compare file contents now;2.refactor another functionality:generate flow features, but no testing now;

--- python/packet_feature.py
+++ python/packet_feature.py
@@ -2,16 +2,26 @@ import subprocess
 import os
 import sys
 
-def tshark(trace_file_name, trace_feature_file_name, print_err=False):
+'''
+    Generate packet features
+'''
+def generate(trace_file_name, trace_feature_file_name, print_err=False):
     if not os.path.exists(trace_feature_file_name):
-        tshark_command = subprocess.Popen('tshark -r {} -Y tcp -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.len -e frame.time_relative -e tcp.seq -e tcp.ack -e tcp.flags.ack -e tcp.flags.syn -e tcp.flags.fin -e tcp.stream -Eheader=y -Eseparator=, > {}'.format(trace_file_name, trace_feature_file_name), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        tshark_command = subprocess.Popen(\
+            'tshark -r {input} -Y tcp -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.len -e frame.time_relative -e tcp.seq -e tcp.ack -e tcp.flags.ack -e tcp.flags.syn -e tcp.flags.fin -e tcp.stream -Eheader=y -Eseparator=, > {output}'.\
+            format(input=trace_file_name, output=trace_feature_file_name),\
+            shell=True,\
+            stdout=subprocess.PIPE,\
+            stderr=subprocess.PIPE\
+        )
         out_data, err_data = tshark_command.communicate()
         out_data, err_data = out_data.decode('utf-8'), err_data.decode('utf-8')
-        print(out_data,flush=True)
+        if out_data == '':
+            print('Conversion done',flush=True)
         if print_err:
             if err_data != '':
                 print(err_data,file=sys.stderr,flush=True)
             else:
                 print('No error',file=sys.stderr,flush=True)
     else:
-        print('Feature file already exists.')
+        print('Packet feature file already exists.')
--- python/tests/test_packet_feature.py
+++ python/tests/test_packet_feature.py
@@ -2,11 +2,14 @@ import unittest
 import glob
 import os
 import sys, io
+import filecmp
 import packet_feature
 from contextlib import redirect_stdout, redirect_stderr
 
-ONE_PKT_PCAP = 'test_data/1_packet.pcap'
-ONE_PKT_CSV = 'test_data/1_packet.csv'
+ONE_PKT_PCAP = os.path.join('test_data','1_packet_tcp.pcapng')
+GENERATED_ONE_PKT_CSV = os.path.join('test_data','temp_1_packet_tcp.csv')
+ONE_PKT_CSV = os.path.join('test_data','1_packet_tcp.csv')
+
 
 ######################################################################
 #  T E S T   C A S E S
@@ -15,18 +18,19 @@ class TestPacketFeature(unittest.TestCase):
     """ Test Cases for extracting packet features """
 
     def setUp(self):
-        pass
+        if os.path.exists(GENERATED_ONE_PKT_CSV):
+            os.remove(GENERATED_ONE_PKT_CSV)
     
     def tearDown(self):
-        for f in glob.glob('test_data/*.csv'):
-            os.remove(f)
+        if os.path.exists(GENERATED_ONE_PKT_CSV):
+            os.remove(GENERATED_ONE_PKT_CSV)
     
     @staticmethod
     def _capture_stdout_stderr(func, times=1, *args):
         # redirect sys.stdout to a buffer
         out, err = io.StringIO(), io.StringIO()
         with redirect_stdout(out), redirect_stderr(err):
-            for i in range(times):
+            for _ in range(times):
                 func(*args)
 
         out_data, err_data = out.getvalue(), err.getvalue()
@@ -38,32 +42,35 @@ class TestPacketFeature(unittest.TestCase):
         # return value
         return out_data, err_data
 
-    def test_tshark_create_file(self):
+    def test_create_csv(self):
         """ Create feature from a pcap file with 1 packet and assert that it exists """
-        out_data, err_data = TestPacketFeature._capture_stdout_stderr(packet_feature.tshark, 1, ONE_PKT_PCAP,ONE_PKT_CSV)
+        out_data, err_data = TestPacketFeature._capture_stdout_stderr(packet_feature.generate, 1, ONE_PKT_PCAP,GENERATED_ONE_PKT_CSV)
 
-        # checkout something has been printed
-        self.assertTrue(out_data == '\n')
+        # check something
+        self.assertTrue(filecmp.cmp(GENERATED_ONE_PKT_CSV, ONE_PKT_CSV,shallow=False))
+        self.assertTrue(out_data == 'Conversion done\n')
         self.assertTrue(err_data == '')
-        self.assertTrue(os.path.exists(ONE_PKT_CSV))
+        self.assertTrue(os.path.exists(GENERATED_ONE_PKT_CSV))
 
-    def test_tshark_output(self):
+    def test_stderr_output(self):
         """ If print_err is set, something gets printed on screen """
-        out_data, err_data = TestPacketFeature._capture_stdout_stderr(packet_feature.tshark, 1, ONE_PKT_PCAP,ONE_PKT_CSV, True)
+        out_data, err_data = TestPacketFeature._capture_stdout_stderr(packet_feature.generate, 1, ONE_PKT_PCAP,GENERATED_ONE_PKT_CSV, True)
 
-        # checkout something has been printed
-        self.assertTrue(out_data == '\n')
+        # check something
+        self.assertTrue(filecmp.cmp(GENERATED_ONE_PKT_CSV, ONE_PKT_CSV,shallow=False))
+        self.assertTrue(out_data == 'Conversion done\n')
         self.assertTrue(err_data == 'No error\n')
     
-    def test_tshark_create_file_twice(self):
+    def test_create_csv_file_twice(self):
         """ Create feature from a pcap file with 1 packet twice and assert that it exists and stdout should be something """
         # redirect sys.stdout to a buffer
-        out_data, err_data = TestPacketFeature._capture_stdout_stderr(packet_feature.tshark, 2, ONE_PKT_PCAP,ONE_PKT_CSV)
+        out_data, err_data = TestPacketFeature._capture_stdout_stderr(packet_feature.generate, 2, ONE_PKT_PCAP,GENERATED_ONE_PKT_CSV)
 
-        # checkout something has been printed
-        self.assertTrue(out_data == '\nFeature file already exists.\n')
+        # check something
+        self.assertTrue(filecmp.cmp(GENERATED_ONE_PKT_CSV, ONE_PKT_CSV,shallow=False))
+        self.assertTrue(out_data == 'Conversion done\nPacket feature file already exists.\n')
         self.assertTrue(err_data == '')
-        self.assertTrue(os.path.exists(ONE_PKT_CSV))
+        self.assertTrue(os.path.exists(GENERATED_ONE_PKT_CSV))
 
 
 ######################################################################
