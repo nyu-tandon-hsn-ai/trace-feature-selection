4ac09b96f31540a01be7b2f22bf0dce4e1fac387
shuo.jiang <jimmyahacker@gmail.com>
2018-04-05 14:36:01 -0400

    refactored packet_feature and flow_feature;try to run tests but failed - to be done;working on generate flow features

--- python/flow_feature.py
+++ python/flow_feature.py
@@ -4,9 +4,9 @@ from tqdm import tqdm
 import functools
 
 
-def calculate_two_way_communication(len_name, df):
+def calculate_two_way_communication(len_name, sampling_rate, upsampled, df):
+    # one-way statistics
     def get_statistical_features(df, criter, feature_name,name_pred):
-        # upsampling
         feature_avg = df[criter][feature_name].mean()
         feature_avg = -1 if pd.isnull(feature_avg) else feature_avg
         feature_min = df[criter][feature_name].min()
@@ -16,26 +16,41 @@ def calculate_two_way_communication(len_name, df):
         feature_std = df[criter][feature_name].std()
         feature_std = -1 if pd.isnull(feature_std) else feature_std
         feature_sum = df[criter][feature_name].sum()
+        # upsampling
         feature_sum = -1 if pd.isnull(feature_sum) else feature_sum / sampling_rate if upsampled else feature_sum
         feature_count = df[criter][feature_name].count()
         feature_count = feature_count / sampling_rate if upsampled else feature_count
         return {'avg('+name_pred+')':feature_avg,'std('+name_pred+')':feature_std,'min('+name_pred+')':feature_min,'max('+name_pred+')':feature_max,'count('+name_pred[0:8]+')':feature_count, 'sum('+name_pred+')':feature_sum}
     
+    # error check
     addrs = list(set(np.append(df['src_addr'].unique(), df['dst_addr'].unique())))
     if len(addrs) != 2:
         raise AssertionError()
+    
+    # get forward features
     stat = get_statistical_features(df, df['src_addr'] == addrs[0],len_name,'forw_pkt_len')
+
+    # get backward features
     stat.update(get_statistical_features(df, df['src_addr'] == addrs[1],len_name,'back_pkt_len'))
+
+    # calculate forward/backward packets ratio
     stat['fb_ratio'] = stat['count(forw_pkt)'] / stat['count(back_pkt)'] if stat['count(back_pkt)'] > 0 else None
     return pd.Series(stat)
 
-def _tshark_delegate(raw_trace_df, stream_name, len_name, sampling_rate, upsampled, max_count_per_flow):
+def _generate_flow_features(raw_trace_df, stream_name, len_name, sampling_rate, upsampled, max_count_per_flow):
     trace_df = raw_trace_df
     flow_df = pd.DataFrame()
+
     # empty flow
     if trace_df.shape[0] == 0:
         return flow_df
-    # upsampling
+
+    # filter out illegal packets
+    # TODO
+    # trace_df['src_addr'] = trace_df.apply(lambda row: None if pd.isnull(row['src_addr']) or not _is_valid_ipv4(row['ip.src']) else row['src_addr'], axis=1)
+    # trace_df['dst_addr'] = trace_df.apply(lambda row: None if pd.isnull(row['dst_addr']) or not _is_valid_ipv4(row['ip.dst']) else row['dst_addr'], axis=1)
+
+    # generate flow-based features
     flow_df['avg(pkt_len)'] = trace_df.groupby(stream_name)[len_name].mean()
     flow_df['stddev(pkt_len)'] = trace_df.groupby(stream_name)[len_name].std().fillna(-1)
     flow_df['min(pkt_len)'] = trace_df.groupby(stream_name)[len_name].min()
@@ -44,55 +59,26 @@ def _tshark_delegate(raw_trace_df, stream_name, len_name, sampling_rate, upsampl
     flow_df['tot_byte'] = trace_df.groupby(stream_name)[len_name].sum()
     flow_df['rel_start'] = trace_df.groupby(stream_name)[len_name].min()
     flow_df['duration'] = trace_df.groupby(stream_name)[len_name].max() - flow_df['rel_start']
+
+    # upsampling or not
     if not upsampled:
         tqdm.pandas(desc='{} samp rate no upsampling'.format(sampling_rate))
     else:
         flow_df['tot_pkt'] /= sampling_rate
         flow_df['tot_byte'] /= sampling_rate
         tqdm.pandas(desc='{} samp rate with upsampling'.format(sampling_rate))
-    two_way_flow_df = trace_df.groupby(stream_name)[[len_name,'src_addr','dst_addr']].progress_apply(functools.partial(calculate_two_way_communication, len_name))
+
+    # generate forward and backward flow-based features
+    two_way_flow_df = trace_df.groupby(stream_name)[[len_name,'src_addr','dst_addr']].progress_apply(functools.partial(calculate_two_way_communication, len_name, sampling_rate, upsampled))
     flow_df = pd.concat([flow_df,two_way_flow_df],axis=1)
     return flow_df
 
 def tcp_generate(raw_trace_df,sampling_rate=1.0,upsampled=False, max_count_per_flow=None):
-    return _tshark_delegate(raw_trace_df, 'tcp.stream',  'tcp.len', sampling_rate, upsampled, max_count_per_flow)
+    return _generate_flow_features(raw_trace_df, 'tcp.stream',  'tcp.len', sampling_rate, upsampled, max_count_per_flow)
 
 def sample_trace(raw_trace_df,sampling_rate):
     import time
     return raw_trace_df.sample(frac=sampling_rate, random_state=int(time.time()))
 
-def udp_generate(raw_trace_df,sampling_rate=1.0,upsampled=False):
-        
-        addrs = list(set(np.append(df['src_addr'].unique(), df['dst_addr'].unique())))
-        if len(addrs) == 1:
-            addrs.append(addrs[0])
-        if len(addrs) != 2:
-            print(addrs)
-            raise AssertionError()
-        stat = get_statistical_features(df, df['src_addr'] == addrs[0],'udp.length','forw_pkt_len')
-        stat.update(get_statistical_features(df, df['src_addr'] == addrs[1],'udp.length','back_pkt_len'))
-        return pd.Series(stat)
-
-    trace_df = raw_trace_df
-    udp_flow_df = pd.DataFrame()
-    # empty flow
-    if trace_df.shape[0] == 0:
-        return udp_flow_df
-    # upsampling
-    udp_flow_df['avg(udp_pkt_len)'] = trace_df.groupby('udp.stream')['udp.length'].mean()
-    udp_flow_df['stddev(udp_pkt_len)'] = trace_df.groupby('udp.stream')['udp.length'].std().fillna(-1)
-    udp_flow_df['min(udp_pkt_len)'] = trace_df.groupby('udp.stream')['udp.length'].min()
-    udp_flow_df['max(udp_pkt_len)'] = trace_df.groupby('udp.stream')['udp.length'].max()
-    udp_flow_df['tot_pkt'] = trace_df.groupby('udp.stream')['udp.length'].count()
-    udp_flow_df['tot_byte'] = trace_df.groupby('udp.stream')['udp.length'].sum()
-    udp_flow_df['rel_start'] = trace_df.groupby('udp.stream')['frame.time_relative'].min()
-    udp_flow_df['duration'] = trace_df.groupby('udp.stream')['frame.time_relative'].max() - udp_flow_df['rel_start']
-    if not upsampled:
-        tqdm.pandas(desc='UDP {} samp rate no upsampling'.format(sampling_rate))
-    else:
-        udp_flow_df['tot_pkt'] /= sampling_rate
-        udp_flow_df['tot_byte'] /= sampling_rate
-        tqdm.pandas(desc='UDP {} samp rate with upsampling'.format(sampling_rate))
-    two_way_flow_df = trace_df.groupby('udp.stream')[['udp.length','src_addr','dst_addr']].progress_apply(calculate_two_way_tcp)
-    udp_flow_df = pd.concat([udp_flow_df,two_way_flow_df],axis=1)
-    return udp_flow_df
+def udp_generate(raw_trace_df,sampling_rate=1.0,upsampled=False, max_count_per_flow=None):
+    return _generate_flow_features(raw_trace_df, 'udp.stream',  'udp.length', sampling_rate, upsampled, max_count_per_flow)
--- python/packet_feature.py
+++ python/packet_feature.py
@@ -10,70 +10,49 @@ def _is_valid_ipv4(ipaddress_v4):
     import ipaddress
     try:
         ipaddress.IPv4Address(ipaddress_v4)
-    except ipaddress.AddressValueError as e:
+    except ipaddress.AddressValueError:
         return False
     return True
+
+def _tshark_extract(tshark_query_str, trace_file_name, trace_feature_file_name, print_err):
+    if not os.path.exists(trace_feature_file_name):
+        tshark_command = subprocess.Popen(\
+            tshark_query_str.\
+            format(input=trace_file_name, output=trace_feature_file_name),\
+            shell=True,\
+            stdout=subprocess.PIPE,\
+            stderr=subprocess.PIPE\
+        )
+        out_data, err_data = tshark_command.communicate()
+        out_data, err_data = out_data.decode('utf-8'), err_data.decode('utf-8')
+        if out_data == '':
+            print('Conversion done',flush=True)
+        if print_err:
+            if err_data != '':
+                print(err_data,file=sys.stderr,flush=True)
+            else:
+                print('No error',file=sys.stderr,flush=True)
+    else:
+        print('Packet feature file already exists.')
+
+def _generate_full_addr(protocol, trace_feature_file_name):
+    # add source and destination address
+    trace_df = pd.read_csv(trace_feature_file_name)
+    trace_df['src_addr'] = trace_df['ip.src'] + ":" + trace_df[protocol + '.srcport'].apply(str)
+    trace_df['dst_addr'] = trace_df['ip.dst'] + ":" + trace_df[protocol + '.dstport'].apply(str)
+    trace_df.to_csv(trace_feature_file_name, index=False)
 
 '''
     Generate TCP packet features
 '''
 def tcp_generate(trace_file_name, trace_feature_file_name, print_err=False):
-    if not os.path.exists(trace_feature_file_name):
-        tshark_command = subprocess.Popen(\
-            'tshark -r {input} -Y tcp -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.len -e frame.time_relative -e tcp.seq -e tcp.ack -e tcp.flags.ack -e tcp.flags.syn -e tcp.flags.fin -e tcp.stream -Eheader=y -Eseparator=, -Equote=d > {output}'.\
-            format(input=trace_file_name, output=trace_feature_file_name),\
-            shell=True,\
-            stdout=subprocess.PIPE,\
-            stderr=subprocess.PIPE\
-        )
-        out_data, err_data = tshark_command.communicate()
-        out_data, err_data = out_data.decode('utf-8'), err_data.decode('utf-8')
-        if out_data == '':
-            print('Conversion done',flush=True)
-        if print_err:
-            if err_data != '':
-                print(err_data,file=sys.stderr,flush=True)
-            else:
-                print('No error',file=sys.stderr,flush=True)
-        
-        # add source and destination address
-        trace_df = pd.read_csv(trace_feature_file_name)
-        trace_df['src_addr'] = trace_df['ip.src'] + ":" + trace_df['tcp.srcport'].apply(str)
-        trace_df['dst_addr'] = trace_df['ip.dst'] + ":" + trace_df['tcp.dstport'].apply(str)
-        trace_df['src_addr'] = trace_df.apply(lambda row: None if pd.isnull(row['src_addr']) or not _is_valid_ipv4(row['ip.src']) else row['src_addr'], axis=1)
-        trace_df['dst_addr'] = trace_df.apply(lambda row: None if pd.isnull(row['dst_addr']) or not _is_valid_ipv4(row['ip.dst']) else row['dst_addr'], axis=1)
-        trace_df.to_csv(trace_feature_file_name, index=False)
-    else:
-        print('Packet feature file already exists.')
+    _tshark_extract('tshark -r {input} -Y tcp -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.len -e frame.time_relative -e tcp.seq -e tcp.ack -e tcp.flags.ack -e tcp.flags.syn -e tcp.flags.fin -e tcp.stream -Eheader=y -Eseparator=, -Equote=d > {output}', trace_file_name, trace_feature_file_name, print_err)
+    _generate_full_addr('tcp', trace_feature_file_name)
 
 '''
     Generate UDP packet features
 '''
 def udp_generate(trace_file_name, trace_feature_file_name, print_err=False):
-    if not os.path.exists(trace_feature_file_name):
-        tshark_command = subprocess.Popen(\
-            'tshark -r {input} -Y udp -T fields -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e udp.length -e frame.time_relative -e udp.stream -Eheader=y -Eseparator=, -Equote=d > {output}'.\
-            format(input=trace_file_name, output=trace_feature_file_name),\
-            shell=True,\
-            stdout=subprocess.PIPE,\
-            stderr=subprocess.PIPE\
-        )
-        out_data, err_data = tshark_command.communicate()
-        out_data, err_data = out_data.decode('utf-8'), err_data.decode('utf-8')
-        if out_data == '':
-            print('Conversion done',flush=True)
-        if print_err:
-            if err_data != '':
-                print(err_data,file=sys.stderr,flush=True)
-            else:
-                print('No error',file=sys.stderr,flush=True)
-        
-        # add source and destination address
-        trace_df = pd.read_csv(trace_feature_file_name)
-        trace_df['src_addr'] = trace_df['ip.src'] + ":" + trace_df['udp.srcport'].apply(str)
-        trace_df['dst_addr'] = trace_df['ip.dst'] + ":" + trace_df['udp.dstport'].apply(str)
-        trace_df['src_addr'] = trace_df.apply(lambda row: None if pd.isnull(row['src_addr']) or not _is_valid_ipv4(row['ip.src']) else row['src_addr'], axis=1)
-        trace_df['dst_addr'] = trace_df.apply(lambda row: None if pd.isnull(row['dst_addr']) or not _is_valid_ipv4(row['ip.dst']) else row['dst_addr'], axis=1)
-        trace_df.to_csv(trace_feature_file_name, index=False)
-    else:
-        print('Packet feature file already exists.')
+    _tshark_extract('tshark -r {input} -Y udp -T fields -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e udp.length -e frame.time_relative -e udp.stream -Eheader=y -Eseparator=, -Equote=d > {output}', trace_file_name, trace_feature_file_name, print_err)
+    _generate_full_addr('udp', trace_feature_file_name)
+    
