d7657ec60e2fe5ea9c8784c2bef4f906994fc18b
Shuo <jimmyahacker@hotmail.com>
2018-06-28 16:17:39 -0400

    * allow generate flows containing different number of packets

--- extract_img_feature.py
+++ extract_img_feature.py
@@ -4,12 +4,12 @@ from os import listdir
 from os.path import isfile, join
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         exit(1)
     path = sys.argv[1]
+    max_pkts_per_flow = int(sys.argv[2])
 
     filenames = [join(path,f) for f in listdir(path) if isfile(join(path, f))]
     print(filenames)
-    tcp_img(filenames, max_pkts_per_flow=5, train_ratio=0.8, compress=True)
-    udp_img(filenames, max_pkts_per_flow=5, train_ratio=0.8, compress=True)
+    tcp_img(filenames, max_pkts_per_flow=max_pkts_per_flow, train_ratio=0.8, compress=True)
 
