5319070ae1cef7e0a2526a458d7abc228e7e3ae5
Shuo <jimmyahacker@hotmail.com>
2018-07-05 14:06:05 -0400

    * update the test data * modify the default behavior of packet feature collection * move nosetests setup back * modify the behaviors of tests accordingly

--- python/packet_feature.py
+++ python/packet_feature.py
@@ -2,27 +2,25 @@ import subprocess
 import os
 import sys
 import pandas as pd
+import argparse
 
 def _tshark_extract(tshark_query_str, trace_file_name, trace_feature_file_name, print_err):
-    if not os.path.exists(trace_feature_file_name):
-        tshark_command = subprocess.Popen(\
-            tshark_query_str.\
-            format(input=trace_file_name, output=trace_feature_file_name),\
-            shell=True,\
-            stdout=subprocess.PIPE,\
-            stderr=subprocess.PIPE\
-        )
-        out_data, err_data = tshark_command.communicate()
-        out_data, err_data = out_data.decode('utf-8'), err_data.decode('utf-8')
-        if out_data == '':
-            print('Conversion done',flush=True)
-        if print_err:
-            if err_data != '':
-                print(err_data,file=sys.stderr,flush=True)
-            else:
-                print('No error',file=sys.stderr,flush=True)
-    else:
-        print('Packet feature file already exists.')
+    tshark_command = subprocess.Popen(\
+        tshark_query_str.\
+        format(input=trace_file_name, output=trace_feature_file_name),\
+        shell=True,\
+        stdout=subprocess.PIPE,\
+        stderr=subprocess.PIPE\
+    )
+    out_data, err_data = tshark_command.communicate()
+    out_data, err_data = out_data.decode('utf-8'), err_data.decode('utf-8')
+    if out_data == '':
+        print('Conversion done',flush=True)
+    if print_err:
+        if err_data != '':
+            print(err_data,file=sys.stderr,flush=True)
+        else:
+            print('No error',file=sys.stderr,flush=True)
 
 def _generate_full_addr(protocol, trace_feature_file_name):
     # add source and destination address
@@ -42,7 +40,7 @@ def _add_tcp_pkt_len(trace_feature_file_name):
 '''
 def tcp_generate(trace_file_name, trace_feature_file_name, print_err=False, is_cluster=False):
     if is_cluster:
-        _tshark_extract('/share/apps/singularity/2.5.1/bin/singularity exec /beegfs/work/public/singularity/wireshark-2.4.2.img tshark -r {input} -Y tcp -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.hdr_len -e tcp.len -e frame.time_relative -e frame.len -e tcp.seq -e tcp.ack -e tcp.flags.ack -e tcp.flags.syn -e tcp.flags.fin -e tcp.stream -Eheader=y -Eseparator=, -Equote=d > {output}', trace_file_name, trace_feature_file_name, print_err)
+        _tshark_extract('/share/apps/singularity/2.5.2/bin/singularity exec /beegfs/work/public/singularity/wireshark-2.4.2.img tshark -r {input} -Y tcp -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.hdr_len -e tcp.len -e frame.time_relative -e frame.len -e tcp.seq -e tcp.ack -e tcp.flags.ack -e tcp.flags.syn -e tcp.flags.fin -e tcp.stream -Eheader=y -Eseparator=, -Equote=d > {output}', trace_file_name, trace_feature_file_name, print_err)
     else:
         _tshark_extract('tshark -r {input} -Y tcp -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.hdr_len -e tcp.len -e frame.time_relative -e frame.len -e tcp.seq -e tcp.ack -e tcp.flags.ack -e tcp.flags.syn -e tcp.flags.fin -e tcp.stream -Eheader=y -Eseparator=, -Equote=d > {output}', trace_file_name, trace_feature_file_name, print_err)
     _generate_full_addr('tcp', trace_feature_file_name)
@@ -53,7 +51,7 @@ def tcp_generate(trace_file_name, trace_feature_file_name, print_err=False, is_c
 '''
 def udp_generate(trace_file_name, trace_feature_file_name, print_err=False, is_cluster=False):
     if is_cluster:
-        _tshark_extract('/share/apps/singularity/2.5.1/bin/singularity exec /beegfs/work/public/singularity/wireshark-2.4.2.img tshark -r {input} -Y udp -T fields -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e udp.length -e frame.time_relative -e frame.len -e udp.stream -Eheader=y -Eseparator=, -Equote=d > {output}', trace_file_name, trace_feature_file_name, print_err)
+        _tshark_extract('/share/apps/singularity/2.5.2/bin/singularity exec /beegfs/work/public/singularity/wireshark-2.4.2.img tshark -r {input} -Y udp -T fields -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e udp.length -e frame.time_relative -e frame.len -e udp.stream -Eheader=y -Eseparator=, -Equote=d > {output}', trace_file_name, trace_feature_file_name, print_err)
     else:
         _tshark_extract('tshark -r {input} -Y udp -T fields -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e udp.length -e frame.time_relative -e frame.len -e udp.stream -Eheader=y -Eseparator=, -Equote=d > {output}', trace_file_name, trace_feature_file_name, print_err)
     _generate_full_addr('udp', trace_feature_file_name)
--- python/tests/test_packet_feature.py
+++ python/tests/test_packet_feature.py
@@ -5,11 +5,12 @@ import sys, io
 import filecmp
 import packet_feature
 from contextlib import redirect_stdout, redirect_stderr
+from functools import partial
 
-ONE_PKT_PCAP = os.path.join('test_data','1_packet_tcp.pcapng')
-GENERATED_ONE_PKT_CSV = os.path.join('test_data','temp_1_packet_tcp.csv')
-ONE_PKT_CSV = os.path.join('test_data','1_packet_tcp.csv')
-
+ONE_PKT_PCAP = os.path.join('tests','test_data','1_packet_tcp.pcapng')
+GENERATED_ONE_PKT_CSV = os.path.join('tests','test_data','temp_1_packet_tcp.csv')
+ONE_PKT_CSV = os.path.join('tests','test_data','1_packet_tcp.csv')
+IS_CLUSTER=os.getenv('IS_CLUSTER', 'False') == 'True'
 
 ######################################################################
 #  T E S T   C A S E S
@@ -44,9 +45,14 @@ class TestPacketFeature(unittest.TestCase):
 
     def test_create_csv(self):
         """ Create feature from a pcap file with 1 packet and assert that it exists """
-        out_data, err_data = TestPacketFeature._capture_stdout_stderr(packet_feature.tcp_generate, 1, ONE_PKT_PCAP,GENERATED_ONE_PKT_CSV)
+        out_data, err_data = TestPacketFeature._capture_stdout_stderr(partial(packet_feature.tcp_generate, is_cluster=IS_CLUSTER), 1, ONE_PKT_PCAP,GENERATED_ONE_PKT_CSV)
 
         # check something
+        import pandas as pd
+        test_csv=pd.read_csv(GENERATED_ONE_PKT_CSV)
+        real_csv=pd.read_csv(ONE_PKT_CSV)
+        print(test_csv)
+        print(real_csv)
         self.assertTrue(filecmp.cmp(GENERATED_ONE_PKT_CSV, ONE_PKT_CSV,shallow=False))
         self.assertTrue(out_data == 'Conversion done\n')
         self.assertTrue(err_data == '')
@@ -54,7 +60,7 @@ class TestPacketFeature(unittest.TestCase):
 
     def test_stderr_output(self):
         """ If print_err is set, something gets printed on screen """
-        out_data, err_data = TestPacketFeature._capture_stdout_stderr(packet_feature.tcp_generate, 1, ONE_PKT_PCAP,GENERATED_ONE_PKT_CSV, True)
+        out_data, err_data = TestPacketFeature._capture_stdout_stderr(partial(packet_feature.tcp_generate, is_cluster=IS_CLUSTER), 1, ONE_PKT_PCAP,GENERATED_ONE_PKT_CSV, True)
 
         # check something
         self.assertTrue(filecmp.cmp(GENERATED_ONE_PKT_CSV, ONE_PKT_CSV,shallow=False))
@@ -64,11 +70,11 @@ class TestPacketFeature(unittest.TestCase):
     def test_create_csv_file_twice(self):
         """ Create feature from a pcap file with 1 packet twice and assert that it exists and stdout should be something """
         # redirect sys.stdout to a buffer
-        out_data, err_data = TestPacketFeature._capture_stdout_stderr(packet_feature.tcp_generate, 2, ONE_PKT_PCAP,GENERATED_ONE_PKT_CSV)
+        out_data, err_data = TestPacketFeature._capture_stdout_stderr(partial(packet_feature.tcp_generate, is_cluster=IS_CLUSTER), 2, ONE_PKT_PCAP,GENERATED_ONE_PKT_CSV)
 
         # check something
         self.assertTrue(filecmp.cmp(GENERATED_ONE_PKT_CSV, ONE_PKT_CSV,shallow=False))
-        self.assertTrue(out_data == 'Conversion done\nPacket feature file already exists.\n')
+        self.assertTrue(out_data == 'Conversion done\nConversion done\n')
         self.assertTrue(err_data == '')
         self.assertTrue(os.path.exists(GENERATED_ONE_PKT_CSV))
 
