dafb06dd4ee90b48023e09bfb4e88be4faf850f7
Shuo <jimmyahacker@hotmail.com>
2018-07-12 14:33:41 -0400

    * could run multiple max_pkts_per_flow at the same time now, but in a serial manner

--- extract_img_feature.py
+++ extract_img_feature.py
@@ -8,12 +8,14 @@ from img_feature import img
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         exit(1)
-    start_time = time.time()
+    tot_start_time = time.time()
     path = sys.argv[1]
-    max_pkts_per_flow = int(sys.argv[2])
+    pkts=list(map(int,sys.argv[2].split(',')))
     label_type = sys.argv[3]
 
     filenames = [join(path,f) for f in listdir(path) if isfile(join(path, f))]
-    img(filenames, max_pkts_per_flow=max_pkts_per_flow, train_ratio=0.8, compress=True, label_type=label_type)
-    print('Time elapsed: {duration} second(s)'.format(duration=time.time() - start_time))
-
+    for max_pkts_per_flow in pkts:
+        start_time = time.time()
+        img(filenames, max_pkts_per_flow=max_pkts_per_flow, train_ratio=0.8, compress=True, label_type=label_type)
+        print('Time elapsed for {pkt}-pkt-sub-flow: {duration} second(s)'.format(pkt=max_pkts_per_flow, duration=time.time() - start_time))
+    print('Total time: {duration} second(s)'.format(duration=time.time() - tot_start_time))
