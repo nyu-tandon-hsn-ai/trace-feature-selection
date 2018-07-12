e934bb793b4ce4952f2f482b472a6d09c73dc3d9
Shuo <jimmyahacker@hotmail.com>
2018-07-12 18:00:02 -0400

    * use decorator to record time

--- extract_img_feature.py
+++ extract_img_feature.py
@@ -5,6 +5,17 @@ from os.path import isfile, join
 
 from img_feature import img
 
+def time_record(func):
+    def func_wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        print('Time elapsed for {pkt}-pkt-sub-flow: {duration} second(s)'.format(pkt=kwargs['max_pkts_per_flow'], duration=time.time() - start_time))
+    return func_wrapper
+
+@time_record
+def my_img(*args, **kwargs):
+    img(*args, **kwargs)
+
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         exit(1)
@@ -15,7 +26,5 @@ if __name__ == '__main__':
 
     filenames = [join(path,f) for f in listdir(path) if isfile(join(path, f))]
     for max_pkts_per_flow in pkts:
-        start_time = time.time()
-        img(filenames, max_pkts_per_flow=max_pkts_per_flow, train_ratio=0.8, compress=True, label_type=label_type)
-        print('Time elapsed for {pkt}-pkt-sub-flow: {duration} second(s)'.format(pkt=max_pkts_per_flow, duration=time.time() - start_time))
+        my_img(filenames, max_pkts_per_flow=max_pkts_per_flow, train_ratio=0.8, compress=True, label_type=label_type)
     print('Total time: {duration} second(s)'.format(duration=time.time() - tot_start_time))
